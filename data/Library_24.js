a9587107843f62a094c7e4152f5bd64e6520f806
camkool <campbellwardnz@gmail.com>
2025-10-26 15:57:55 +1300

    Regions

---
+++ data/Library_24.js
@@ -0,0 +1,1 @@
+var json_Library_24 = {"type":"FeatureCollection","name":"Library_24","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/Museums_25.js
@@ -0,0 +1,1 @@
+var json_Museums_25 = {"type":"FeatureCollection","name":"Museums_25","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
